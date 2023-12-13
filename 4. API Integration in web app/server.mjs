f44e533d2d980b6a284a12bcc3e68b451932c45e
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-13 23:37:03 +0500

    final

--- 4. API Integration in web app/server.mjs
+++ 4. API Integration in web app/server.mjs
@@ -2,7 +2,7 @@
 
 import express from 'express'
 const app = express()
-const port = 3000
+const port = 5001
 import path from 'path'
 
 app.get('/weather', (req, res) => {
@@ -14,13 +14,9 @@ app.get('/weather', (req, res) => {
 const __dirname = path.resolve();
 console.log("__dirname:", __dirname)
 console.log("__dirname:", path.join(__dirname, "/web"))
-    // 8. app.get('/', express.static(path.join(__dirname,"/web")))
-    //  app.use('/*', express.static(path.join(__dirname,"/web")))
 
-
-app.use('/', express.static(path.join(__dirname, 'public')))
-app.use('/', express.static(path.join(__dirname, 'public')))
-
+app.get('/', express.static(path.join(__dirname,"/web")))
+app.use('/*', express.static(path.join(__dirname,"/web")))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
